71ed58ee84a578648489c345ada2d5f1ad5ec4d9
Diprati Das <dipratidas2004@gmail.com>
2024-09-23 13:13:23 +0530

    create login.ejs and register.ejs, update index.js

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ import bodyParser from "body-parser";
 import {dirname} from "path";
 import {fileURLToPath} from "url";
 import pg from "pg";
+import bcrypt from "bcrypt";
+import passport from "passport";
+import { Strategy } from "passport-local";
+import GoogleStrategy from "passport-google-oauth2";
+import session from "express-session";
 import 'dotenv/config';
 
 
